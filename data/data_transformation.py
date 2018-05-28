a3a1b7fdf35d6fd92f86cd28b51e0908c7d4094a
Albin <albin.larsson@raa.se>
2018-05-28 14:17:04 +0200

    add a upper limit to enchanted descriptions

--- data/data_transformation.py
+++ data/data_transformation.py
@@ -8,7 +8,10 @@ def get_license_from_uri(uri: str) -> Union[object, bool]:
 def get_shortified_description(desc: str) -> str:
     def too_short(sentence: str) -> bool:
         return True if len(sentence) < 45 else False
-    
+
+    def too_long(sentence: str) -> bool:
+        return True if len(sentence) > 160 else False
+
     def does_start_with_slash(para: str) -> bool:
         return True if para.startswith('/') else False
 
@@ -22,10 +25,15 @@ def get_shortified_description(desc: str) -> str:
     for pragraph in pragraphs:
         sentences = pragraph.split('. ')
         sentences = filterfalse(too_short, sentences)
+        sentences = filterfalse(too_long, sentences)
         sentences = filterfalse(is_list_item, sentences)
         for sentence in sentences:
-            if sentence
-                return sentence + '.'
+            if sentence:
+                if not sentence.endswith('.'):
+                    sentence = sentence + '.'
+
+                return sentence
+
     #TODO how to deal with fails?
     return ''
 
