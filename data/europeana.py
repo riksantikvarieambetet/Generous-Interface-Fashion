cc9e23bf968ad7c01191082962f47be2aa471d55
Albin <albin.larsson@raa.se>
2018-07-02 12:48:46 +0200

    europeana generic generator

--- data/europeana.py
+++ data/europeana.py
@@ -6,12 +6,17 @@ class Europeana:
         self.key = key
 
     def provider_subject_generator(self, provider, subject):
-        start_query = 'https://www.europeana.eu/api/v2/search.json?wskey={0}&query=what:"{1}"&qf=DATA_PROVIDER:"{2}"&thumbnail=true&media=true&rows=1'.format(self.key, subject, provider)
+        items = self.generic_query_generator('query=what:"{0}"&qf=DATA_PROVIDER:"{1}"&thumbnail=true&media=true'.format(subject, provider))
+        for item in items:
+            yield item
+
+    def generic_query_generator(self, query):
+        start_query = 'https://www.europeana.eu/api/v2/search.json?wskey={0}&{1}&rows=1'.format(self.key, query)
         r = requests.get(start_query)
         data = r.json()
         required_n_requests = math.ceil(data['totalResults'] / 100)
 
-        query = 'https://www.europeana.eu/api/v2/search.json?wskey={0}&query=what:"{1}"&qf=DATA_PROVIDER:"{2}"&thumbnail=true&media=true&rows=100'.format(self.key, subject, provider)
+        query = 'https://www.europeana.eu/api/v2/search.json?wskey={0}&{1}&rows=100'.format(self.key, query)
         count = 0
         while required_n_requests > count:
             start_record = count * 100
