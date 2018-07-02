310192349c4f2a0cfcbf75b1f7367a25931c5d96
Albin <albin.larsson@raa.se>
2018-07-02 11:55:20 +0200

    add Google Vision caching

--- data/google_vision.py
+++ data/google_vision.py
@@ -1,18 +1,32 @@
+import shelve
+
 from google.oauth2 import service_account
 from google.cloud import vision
 
 from colour import Color
 
 class GoogleVision:
-    def __init__(self, service_account_file):
+    def __init__(self, service_account_file, cache=True):
         credentials = service_account.Credentials.from_service_account_file(service_account_file)
         self.client = vision.ImageAnnotatorClient(credentials=credentials)
+        self.cache = cache
 
     def get_colors(self, url):
         colors = []
-        response = self.client.image_properties({
-            'source': { 'image_uri': url }
-        })
+
+        if self.cache:
+            cache = shelve.open('google_vision_color_cache')
+
+        if self.cache and url in cache:
+            response = cache[url]
+        else:
+            response = self.client.image_properties({
+                'source': { 'image_uri': url }
+            })
+            cache[url] = response
+
+        if self.cache:
+            cache.close()
 
         for rank in response.image_properties_annotation.dominant_colors.colors:
             if rank.score < 0.01:
