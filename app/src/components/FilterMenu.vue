4f376f68d2dfa44332ce7f22e41b5e6b635057e1
Abbe98 <albin.post@gmail.com>
2018-07-10 10:54:47 +0200

    hide "look color" btn if no color is active

--- app/src/components/FilterMenu.vue
+++ app/src/components/FilterMenu.vue
@@ -10,7 +10,7 @@
       <FilterContainer v-if="colorFilterOpen" v-hammer:swipe.up="toggleColorFilter">
         <div class="desktop-break">
           <Chrome v-bind:value="currentColor" v-on:input="updateColorFilterDynamic" v-bind:disableAlpha="true" v-bind:disableFields="true" class="color-picker" />
-          <button v-on:click="lockColor">Lock color</button>
+          <button v-if="currentColor != `url('transparent.png')`" v-on:click="lockColor">Lock color</button>
         </div>
         <div class="desktop-break">
           <div v-for="color in staticColors" v-bind:key="color" v-bind:style="{ background: color }" class="color"><span role="button" v-on:click="removeColor(color)">x</span></div>
