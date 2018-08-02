5045efe595b77069ee6534dfa84e21c45b2b26bb
Abbe98 <albin.post@gmail.com>
2018-08-02 15:04:29 +0200

    clean up

--- app/src/components/FilterMenu.vue
+++ app/src/components/FilterMenu.vue
@@ -131,9 +131,6 @@ export default {
 
       this.executeFiltering();
       this.setselectedColorId(Math.min(this.selectedColorId, this.staticColors.length - 1));
-      /* if(this.selectedColorId == this.staticColors.length - 1 && this.selectedColorId != 0){
-        this.selectedColorId--;
-      } */
     },
 
     setselectedColorId(id) {
@@ -173,31 +170,6 @@ export default {
       console.log('debug: reseting visibleLimit');
       store.commit('resetVisibleLimit');
       window.scrollTo(0, 0);
-    },
-
-    isSimilarColor(hex1, hex2) {
-      if (hex1.startsWith('#')) hex1 = hex1.substring(1);
-      if (hex2.startsWith('#')) hex2 = hex2.substring(1);
-
-      const r1 = parseInt(hex1.substring(0, 2), 16);
-      const g1 = parseInt(hex1.substring(2, 4), 16);
-      const b1 = parseInt(hex1.substring(4, 6), 16);
-
-      const r2 = parseInt(hex2.substring(0, 2), 16);
-      const g2 = parseInt(hex2.substring(2, 4), 16);
-      const b2 = parseInt(hex2.substring(4, 6), 16);
-
-      let r = 255 - Math.abs(r1 - r2);
-      let g = 255 - Math.abs(g1 - g2);
-      let b = 255 - Math.abs(b1 - b2);
-
-      r /= 255;
-      g /= 255;
-      b /= 255;
-
-      const score = (r + g + b) / 3;
-
-      return (score >= 0.9);
     },
 
     isSimilarHSV(hex1, hex2) {
