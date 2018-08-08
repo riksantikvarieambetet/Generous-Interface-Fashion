3ac0dba9b0ff3bd629ff741f2eb51bbab4544d7a
Abbe98 <albin.post@gmail.com>
2018-08-08 13:50:59 +0200

    change click bahavior on labels

--- app/src/components/LabelStack.vue
+++ app/src/components/LabelStack.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <span v-for="label in labels" v-bind:key="label[0]" v-bind:style="{ fontSize: label[1] }" @click="setSelectedLabelId(label[0])">
-      <span :class="{ selectedLabel: selectedLabelIds.includes(label[0]) }">{{ label[0] }}</span>
+    <span v-for="label in labels" v-bind:key="label[0]" v-bind:style="{ fontSize: label[1] }">
+      <span @click="setSelectedLabelId(label[0])" class="label" :class="{ selectedLabel: selectedLabelIds.includes(label[0]) }">{{ label[0] }}</span>
       <span>, </span>
     </span>
   </div>
@@ -32,6 +32,10 @@ export default {
 </script>
 
 <style scoped>
+.label {
+    cursor: pointer;
+}
+
 .selectedLabel {
     background: #008cff;
     padding: 0px 2px;
