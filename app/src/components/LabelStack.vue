8eaf4065a5edf4b515963afabaddea2c0c88a487
Abbe98 <albin.post@gmail.com>
2018-08-08 13:19:07 +0200

    add min and max values to label items

--- app/src/components/LabelStack.vue
+++ app/src/components/LabelStack.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <span v-for="label in labels" v-bind:key="label[0]" v-bind:style="{ fontSize: label[1] / 10 + 'px' }" @click="setSelectedLabelId(label[0])" :class="{ selectedLabel: selectedLabelIds.includes(label[0]) }">{{ label[0] }}, </span>
+    <span v-for="label in scaledLabels" v-bind:key="label[0]" v-bind:style="{ fontSize: label[1] }" @click="setSelectedLabelId(label[0])" :class="{ selectedLabel: selectedLabelIds.includes(label[0]) }">{{ label[0] }}, </span>
   </div>
 </template>
 
@@ -17,6 +17,17 @@ export default {
   computed: {
     selectedLabelIds() {
       return store.state.selectedLabelIds;
+    },
+
+    scaledLabels() {
+      let labels = this.labels.map(l => {
+        l[1] = l[1] / 10;
+        if (l[1] < 10) l[1] = 10;
+        if (l[1] > 40) l[1] = 40;
+        l[1] += 'px';
+        return l;
+      });
+      return labels;
     },
   },
   methods: {
