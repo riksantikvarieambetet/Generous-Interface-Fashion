9e1fcb7c6a6eb3c6b009891799a2004a4a369f9f
Abbe98 <albin.post@gmail.com>
2018-08-16 19:43:54 +0200

    helper text in color mountain

--- app/src/components/ColorMountain.vue
+++ app/src/components/ColorMountain.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="color-mountain">
+    <p>Filtrera innehållet med ett färgkrav.</p>
     <div v-for="c in sortedColors" v-bind:key="c[0]" v-bind:style="{ background: c[0], height: c[1] + 'px', width: 100 / colors.length + '%' }" @click="setSelectedSnappedColorId(c[0])" :class="{ selected: selectedSnappedColorIds.includes(c[0]) }"></div>
   </div>
 </template>
@@ -41,11 +42,26 @@ export default {
     overflow: hidden;
 }
 
+p {
+    position: absolute;
+    font-size: 3vw;
+    transform: scaleY(-1);
+    right: 60px;
+    top: 120px;
+}
+
 .color-mountain > div {
     float: left;
     min-height: 10px;
     max-height: 250px;
     cursor: pointer;
     position: relative;
+}
+
+@media (max-width: 400px) {
+  p {
+    font-size: 14px;
+    right: 35px;
+  }
 }
 </style>
